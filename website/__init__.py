6e3caaf046707b83b5ae4a061c776bd813375385
Severin <odics@vcu.edu>
2023-02-12 13:51:42 -0500

    Added flask-migrate to the project for easier DB changes.

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 DB_NAME = "database.db"
 
 
@@ -11,6 +13,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'sdf897sdf987'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from .views import views
     from .auth import auth
